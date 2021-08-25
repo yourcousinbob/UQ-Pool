c2b413e9e2f1bf107ffbc8f9d47de52d0a2eccb3
nathanhettige <67953260+nathanhettige@users.noreply.github.com>
2021-08-25 17:13:48 +1000

    Added KeyboardAvoidingView to App

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform} from 'react-native';
 import { Provider } from "react-redux";
 import { store } from './store';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -19,29 +19,27 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <Stack.Navigator>
-            <Stack.Screen 
-              name='HomeScreen'
-              component={HomeScreen}
-              options={{headerShown:false}}
-            />
-            <Stack.Screen 
-              name='RiderScreen'
-              component={RiderScreen}
-              options={{headerShown:false}}
-            />
-          </Stack.Navigator>
+          <KeyboardAvoidingView 
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={{flex: 1}}
+            keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+          >
+            <Stack.Navigator>
+              <Stack.Screen 
+                name='HomeScreen'
+                component={HomeScreen}
+                options={{headerShown:false}}
+              />
+              <Stack.Screen 
+                name='RiderScreen'
+                component={RiderScreen}
+                options={{headerShown:false}}
+              />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
