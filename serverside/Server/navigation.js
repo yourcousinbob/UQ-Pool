101a579f8decab65f3316b2607f2bd6594dbb2ee
Bobby Melhem <bobbyjacksonmelhem@gmail.com>
2021-09-05 20:59:26 +1000

    asdasd

--- serverside/Server/navigation.js
+++ serverside/Server/navigation.js
@@ -5,10 +5,10 @@ const getTravelTime = async (origin, destination) => {
     const response = await fetch('https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&region=au&origins='+origin+'&destinations='+destination+'&key='+key)
     .then((res) => res.json()).then((data) => {
         console.log(data)
-        return response.resolve(parseInt(data.rows[0].elements[0].duration.text));
     }).catch((err) => {
         console.log('rejected', err)
     });
+    return response.resolve(parseInt(data.rows[0].elements[0].duration.text));
 };
 
 module.exports = {
