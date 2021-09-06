595bae6bc5c74d14055e1ecf8d53d52346acf259
Bobby Melhem <bobbyjacksonmelhem@gmail.com>
2021-09-06 22:41:30 +1000

    revert 2

--- serverside/Server/navigation.js
+++ serverside/Server/navigation.js
@@ -3,9 +3,7 @@ const pool = require('./dbPool');
 const getTravelTime = async (origin, destination) => {
     const key = process.env.GOOGLE_MAPS_API_KEY
     const response = await fetch('https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&region=au&origins='+origin+'&destinations='+destination+'&key='+key)
-    const data = await response.json()
-    console.log(data)
-    return parseInt(data.rows[0].elements[0].duration.text);
+    return response.json().then(data => {parseInt(data.rows[0].elements[0].duration.text)});
 };
 
 
