2417b5ef2d491b667c4c8bf036d8c4a688f2b010
Bobby Melhem <bobbyjacksonmelhem@gmail.com>
2021-09-06 22:00:38 +1000

    Trying to fix async code

--- serverside/Server/navigation.js
+++ serverside/Server/navigation.js
@@ -3,10 +3,11 @@ const pool = require('./dbPool');
 
 const getTravelTime = async (origin, destination) => {
     const key = process.env.GOOGLE_MAPS_API_KEY
-    const response = await fetch('https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&region=au&origins='+origin+'&destinations='+destination+'&key='+key)
-    const data = await response.json();
-    return parseInt(data.rows[0].elements[0].duration.text);
+    return fetch('https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&region=au&origins='+origin+'&destinations='+destination+'&key='+key)
+    .then(response => {response.json()})
+    .then(data => {parseInt(data.rows[0].elements[0].duration.text)});
 };
+
 
 module.exports = {
     getTravelTime,
