eefb26bb0c9e44f15d068bef6241a33c80cc073a
Yuma Antoine Decaux <antoine@oseyeris.com>
2021-09-08 18:06:47 +1000

    background location refresh + location request for android added in ./components

---
+++ clientside/components/LocationRequest.js
@@ -0,0 +1,28 @@
+/* based on the blog:
+https://javascript.plainenglish.io/react-native-foreground-service-f7fc8e617fba
+
+The below is required to not timeout when the app is in background mode.
+
+More info on:
+https://itnext.io/react-native-background-location-tracking-without-timeout-and-with-app-killed-3dbfbc80ad01
+*/
+
+
+import { PermissionsAndroid } from 'react-native';
+
+//request the permission before starting the service.
+const backgroundgranted = await PermissionsAndroid.request(
+  PermissionsAndroid.PERMISSIONS.ACCESS_BACKGROUND_LOCATION,
+  {
+    title: 'Background Location Permission',
+    message:
+      'UQ-pool requires location to allow riders and drivers to find each other' +
+      'and get live quality updates.',
+    buttonNeutral: 'Ask Me Later',
+    buttonNegative: 'Cancel',
+    buttonPositive: 'OK',
+  },
+);
+if (backgroundgranted === PermissionsAndroid.RESULTS.GRANTED) {
+  //do your thing!
+}
