0d16af465cb7f0561b034ae794c9286546d31d1e
nathanhettige <67953260+nathanhettige@users.noreply.github.com>
2021-10-02 16:30:35 +1000

    Created a new homescreen (testing location)

---
+++ clientside/screens/MapScreen.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import { Text, StyleSheet, View, PermissionsAndroid } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { BOX } from "../stylesheets/theme";
+import * as Location from "expo-location";
+
+export default function MapScreen() {
+  const [latitude, setLatitude] = useState(-27.497);
+  const [longitude, setLongitude] = useState(153.0134);
+  const latitudeDelta = 0.005;
+  const longitudeDelta = 0.005;
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        alert("Permission to access foreground location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.Lowest,
+      });
+      console.log(location);
+      setLatitude(location.coords.latitude);
+      setLongitude(location.coords.longitude);
+    })();
+  }, []);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <MapView
+        style={styles.map}
+        showsUserLocation={true}
+        region={{
+          latitude: latitude,
+          longitude: longitude,
+          longitudeDelta: longitudeDelta,
+          latitudeDelta: latitudeDelta,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: latitude,
+            longitude: longitude,
+          }}
+        />
+      </MapView>
+      <View style={styles.bubble}>
+        <Text>Text inside the bubble</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  map: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    zIndex: -1,
+  },
+  bubble: {
+    backgroundColor: "white",
+    borderRadius: BOX.borderRadius,
+    marginHorizontal: "10%",
+    padding: 15,
+    width: "80%",
+    height: 120,
+    position: "absolute",
+    bottom: "15%",
+  },
+});
