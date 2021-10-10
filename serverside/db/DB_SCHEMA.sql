b0a76ffcc13052bafa84df3b864959dab3265bc8
jahames <knockknockitsjimmy@outlook.com>
2021-10-10 13:03:01 +1000

    added reward cost to db

--- serverside/db/DB_SCHEMA.sql
+++ serverside/db/DB_SCHEMA.sql
@@ -69,6 +69,7 @@ FOREIGN KEY (route_id) REFERENCES route(route_id) ON DELETE CASCADE);
 
 CREATE TABLE rewards (
 reward_id varchar(10) NOT NULL UNIQUE,
+cost varchar(10),
 description varchar(100),
 image varchar(100));
 
