20b0a0493010cef9fc6318d5a751ad2b8a56c095
jahames <knockknockitsjimmy@outlook.com>
2021-10-10 15:44:24 +1000

    Get rewards python test

--- serverside/db/tests/getRewards.py
+++ serverside/db/tests/getRewards.py
@@ -3,21 +3,10 @@ import requests as R
 
 s = R.session()
 url = 'https://uqpool.xyz:7777/rewards'
+headers = {'content-type': 'application/json'}
 
 if __name__ == "__main__":
     print('user exists test')
-    content = s.post(url, json=data, headers=headers).content
-    payload = j.loads(content)
-    print(payload)
-    print('invalid email test')
-    content = s.post(url, json=data2, headers=headers).content
-    payload = j.loads(content)
-    print(payload)
-    print('wrong digit number test')
-    content = s.post(url, json=data3, headers=headers).content
-    payload = j.loads(content)
-    print(payload)
-    print('success test')
-    content = s.post(url, json=data4, headers=headers).content
+    content = s.get(url).content
     payload = j.loads(content)
     print(payload)
