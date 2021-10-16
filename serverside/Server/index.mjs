f0a26a81227f619ae50b611d9e4318052b9e098f
James Robins <knockknockitsjimmy@outlook.com>
2021-10-16 13:48:23 +1000

    modified socketio request for adding a driver

--- serverside/Server/index.mjs
+++ serverside/Server/index.mjs
@@ -267,9 +267,10 @@ io.on('connection', async (socket) => {
 
    //Driver
    socket.on('add', (body) => {
-        console.log("Adding driver to active drivers: " + body.sid);
-        book.addDriver(body, function (payload) {
-            connected[body.sid].emit('add', JSON.stringify(payload));
+        let msg = JSON.parse(body)
+        console.log("Adding driver to active drivers: " + msg.sid);
+        book.addDriver(msg, function (payload) {
+            connected[msg.sid].emit('add', JSON.stringify(payload));
         });
    });
 
