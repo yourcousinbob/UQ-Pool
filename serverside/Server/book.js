35de2e5eb467eb4c24de8fbacfdc4502e0368d3e
Bobby Melhem <bobbyjacksonmelhem@gmail.com>
2021-10-16 18:08:35 +1000

    test 2

--- serverside/Server/book.js
+++ serverside/Server/book.js
@@ -56,7 +56,7 @@ module.exports = {
                 console.log("Could not connect to server")
                 throw err;
             }
-            con.query("SELECT first_name, last_name, image FROM user WHERE sid='"+body[0]+"';", (err, rows) => {
+            return con.query("SELECT first_name, last_name, image FROM user WHERE sid='"+body[0]+"';", (err, rows) => {
                 if(err) {
                     console.log("Could not pass query")
                     throw err;
