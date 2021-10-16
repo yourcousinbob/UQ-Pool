bef58c4a99471eb5fac25283fdc17a7940314272
Bobby Melhem <bobbyjacksonmelhem@gmail.com>
2021-10-16 12:49:24 +1000

    Added extra information to driver list and formatted better.

--- serverside/Server/book.js
+++ serverside/Server/book.js
@@ -24,9 +24,8 @@ module.exports = {
                 if (rows.length < 1) {
                     console.log("No available drivers");
                 } else {
+                    drivers = []
                     driver_heuristics = [];
-                    //Distance calc assuming all entries sound might
-                    //be better way to do async tried lots fix if u can.
 
                     async function getDetour (driver_heuristics, rows) {
                         for (let i = 0; i < rows.length; i++) {
@@ -34,18 +33,33 @@ module.exports = {
                             pickupETA = await navigation.getTravelTime(rows[i].location, body.location);
                             detourETA = await navigation.getTravelTime(body.location, body.destination) 
                             heuristic = pickupETA + detourETA - driverETA;
-                            driver_heuristics.push([rows[i].registration, heuristic])
+                            driver_heuristics.push([rows[i].driver_id, heuristic, rows.registration, info.first_name, info.last_name, image])
+                            driver_heuristics.sort((first, second) => {
+                                return first[1] - second[1];
+                            });
                         }
-                        driver_heuristics.sort((first, second) => {
-                            return first[1] - second[1];
-                        });
                     }
 
-                        console.log("Successfully parsed drivers for " + body.sid);
-                        getDetour(driver_heuristics, rows).then(response => {
-                            console.log(driver_heuristics);
-                            result(driver_heuristics)
+                    console.log("Successfully parsed drivers for " + body.sid);
+                    getDetour(driver_heuristics, rows).then(response => {
+                        con.query("SELECT first_name, last_name, image FROM user WHERE driver_id='"+JSON.stringify(rows[i].driver_id)+"';", (err,info) => {
+                            if(err) {
+                                console.log("Could not pass query")
+                                throw err;
+                            }
+                            for (let i = 0; i < driver_heuristics.length; i++){
+                                drivers.push({
+                                    registration: driver_heuristics[i][2], 
+                                    heuristic: driver_heuristics[i][1], 
+                                    first_name: driver_heuristics[i][3], 
+                                    last_name: driver_heuristics[i][4], 
+                                    image: driver_heuristics[i][5]
+                                })
+                            }
                         });
+                        console.log(drivers);
+                        result(JSON.stringify(drivers))
+                    });
                 };
             });
             con.release((err) => {
