b6113c293237c91ede9ecc422b11c1b3a6b8b82e
nathanhettige <67953260+nathanhettige@users.noreply.github.com>
2021-10-16 21:53:35 +1000

    Styled RewardScreen

--- clientside/screens/RewardScreen.js
+++ clientside/screens/RewardScreen.js
@@ -1,42 +1,107 @@
-import React from "react";
-import { Dimensions, StyleSheet, Text, View } from "react-native";
-import { fonts } from "react-native-elements/dist/config";
-import { ScreenWidth } from "react-native-elements/dist/helpers";
-import { min } from "react-native-reanimated";
+import React, { useEffect, useState } from "react";
+import {
+	Dimensions,
+	StyleSheet,
+	Text,
+	View,
+	FlatList,
+	Image,
+	TouchableOpacity,
+	SectionList,
+	Alert,
+	ScrollView,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { box, BOX, COLORS, FONT_SIZE } from "../stylesheets/theme";
 
 const Redeem = () => {
-    Alert.alert('Success', 'Please get the following barcode scanned to claim your reward!')
-    /*Display a random qr/bar code image, with a button saying finished HERE*/
-  }
+	Alert.alert(
+		"Success",
+		"Please get the following barcode scanned to claim your reward!"
+	);
+	/*Display a random qr/bar code image, with a button saying finished HERE*/
+};
 
-const RewardScreen = () => {
+const RewardsHeader = () => {
 	return (
-		<View
-			style={{ backgroundColor: COLORS.primary, width: "100%", height: "100%" }}
-		>
-			<View style={styles.header}>
-				<SafeAreaView style={{width: Dimensions.get("window").width}}>
-					<View
-						style={{
-							// backgroundColor: "red",
-							justifyContent: "center",
-							alignItems: "center",
-                            height: "70%"
-						}}
-					>
-						<Text style={{ fontSize: FONT_SIZE.title, color: COLORS.primary }}>
-							Rewards
+		<View style={styles.header}>
+			<SafeAreaView style={{ width: Dimensions.get("window").width }}>
+				<View
+					style={{
+						justifyContent: "center",
+						alignItems: "center",
+						height: "60%",
+					}}
+				>
+					<Text style={{ fontSize: FONT_SIZE.title, color: COLORS.primary }}>
+						Rewards
+					</Text>
+				</View>
+				<View
+					style={{ height: "40%", display: "flex", alignItems: "center" }}
+				>
+					<View style={[box.shadows, styles.pointsDisplay]}>
+						<Text style={{ fontSize: FONT_SIZE.text, color: "white" }}>
+							Points: 80
 						</Text>
 					</View>
-					<View style={{height: "30%", display: "flex", alignItems: "flex-start"}}>
-						<View style={[styles.pointsDisplay, box.shadows]}>
-							<Text style={{ fontSize: FONT_SIZE.text, color: "white" }}>Points: 80</Text>
-						</View>
-					</View>
-				</SafeAreaView>
-			</View>
+				</View>
+			</SafeAreaView>
+		</View>
+	);
+};
+
+const RewardScreen = () => {
+	const [rewards, setRewards] = useState(null);
+
+	useEffect(() => {
+		const getRewards = async () => {
+			try {
+				const response = await fetch("https://uqpool.xyz:7777/rewards", {
+					method: "GET",
+					headers: {
+						accept: "application/json",
+						"Content-Type": "application/json",
+					},
+				});
+				const json = await response.json();
+				setRewards(json.rewards);
+				console.log(json.rewards)
+			} catch (error) {
+				console.log("Caught error when retreiving rewards");
+				console.log(error);
+			}
+		};
+
+		getRewards();
+	}, []);
+
+	return (
+		<View style={{ backgroundColor: COLORS.primary, height: "100%" }}>
+			<FlatList
+				scrollIndicatorInsets={{ right: 1 }}
+				ListHeaderComponent={<RewardsHeader />}
+				columnWrapperStyle={{
+					justifyContent: "space-between",
+					marginHorizontal: 10,
+				}}
+				data={rewards}
+				keyExtractor={(item) => {
+					return item.reward_id;
+				}}
+				numColumns={2}
+				renderItem={(post) => {
+					const item = post.item;
+					return (
+						<TouchableOpacity style={styles.item} onPress={() => Redeem()}>
+							<Text style={styles.itemVendor}>{item.vendor}</Text>
+							<Text style={styles.itemDescription}>{item.description}</Text>
+							<Image style={styles.itemImg} source={{ uri: item.image }} />
+							<Text style={styles.itemCost}>{item.cost} Points</Text>
+						</TouchableOpacity>
+					);
+				}}
+			/>
 		</View>
 	);
 };
@@ -46,18 +111,48 @@ export default RewardScreen;
 const styles = StyleSheet.create({
 	header: {
 		backgroundColor: "white",
-		height: 260,
+		height: 220,
 		justifyContent: "center",
 		alignItems: "center",
 		borderBottomEndRadius: BOX.borderRadius,
 		borderBottomStartRadius: BOX.borderRadius,
+		marginBottom: 10,
 	},
 	pointsDisplay: {
 		backgroundColor: COLORS.primary,
-        justifyContent: "center",
-        alignItems:"center",
+		justifyContent: "center",
+		alignItems: "center",
 		borderRadius: BOX.borderRadius,
-        padding: 15,
-        marginLeft: 15
+		paddingVertical: 7,
+		paddingHorizontal: 15,
+	},
+	/****** item box design**********/
+	item: {
+		height: 250,
+		marginBottom: 10,
+		backgroundColor: "white",
+		width: "49%",
+		borderRadius: BOX.borderRadius,
+		padding: box.base.padding,
+	},
+	itemVendor: {
+		fontSize: FONT_SIZE.heading3,
+		fontWeight: "700",
+		marginBottom: 3,
+	},
+	itemDescription: {
+		fontSize: FONT_SIZE.text,
+		marginBottom: 3,
+	},
+	itemImg: {
+		borderRadius: BOX.borderRadius,
+		height: "100%",
+		flex: 1,
+		marginBottom: 3,
+	},
+	itemCost: {
+		fontSize: 15,
+		color: "green",
+		textAlign: "center"
 	},
 });
