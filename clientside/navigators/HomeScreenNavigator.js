7d336f05e9f500b44e30a2ca32aa83cf0a101131
nathanhettige <67953260+nathanhettige@users.noreply.github.com>
2021-10-16 12:09:34 +1000

    Styling and directory cleanup

--- clientside/navigators/HomeScreenNavigator.js
+++ clientside/navigators/HomeScreenNavigator.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 /* Screens */
-import HomeScreen from '../screens/HomeScreen';
-import SupportScreen from '../screens/SupportScreen';
-import RewardScreen from '../screens/RewardScreen';
-import RideHistoryScreen from '../screens/RideHistoryScreen';
-import ChatScreen from '../screens/ChatScreen';
-import BecomeDriverScreen from '../screens/BecomeDriverScreen';
-
+import HomeScreen from "../screens/HomeScreen";
+import SupportScreen from "../screens/SupportScreen";
+import RewardScreen from "../screens/RewardScreen";
+import RideHistoryScreen from "../screens/RideHistoryScreen";
+import ChatScreen from "../screens/ChatScreen";
+import BecomeDriverScreen from "../screens/BecomeDriverScreen";
 
 export default function HomeScreenNavigator() {
-    const Drawer = createDrawerNavigator();
-    return (
-        
-        
-        <Drawer.Navigator initialRouteName="Home">
-            <Drawer.Screen name="Home" component={HomeScreen} />
-            <Drawer.Screen name="Ride History" component={RideHistoryScreen} />
-            <Drawer.Screen name="Reward" component={RewardScreen} />
-            <Drawer.Screen name="Support" component={SupportScreen} />
-            <Drawer.Screen name="Chat" component={ChatScreen} />
-            <Drawer.Screen name="Become A Driver" component={BecomeDriverScreen} />
-        </Drawer.Navigator>
-    );
+	const Drawer = createDrawerNavigator();
+	return (
+		<Drawer.Navigator initialRouteName='Home'>
+			<Drawer.Screen name='Home' component={HomeScreen} />
+			<Drawer.Screen name='Ride History' component={RideHistoryScreen} />
+			<Drawer.Screen name='Reward' component={RewardScreen} />
+			<Drawer.Screen name='Support' component={SupportScreen} />
+			<Drawer.Screen name='Chat' component={ChatScreen} />
+			<Drawer.Screen name='Become A Driver' component={BecomeDriverScreen} />
+		</Drawer.Navigator>
+	);
 }
