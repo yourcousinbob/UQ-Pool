15a7d3c79c6203f6c50d68a84c97f096d3a50601
Yuma Antoine Decaux <antoine@oseyeris.com>
2021-10-22 07:41:31 +1000

    added stub cases for sid/password authenticate page automation + consent automation

--- clientside/components/SSOWebView.js
+++ clientside/components/SSOWebView.js
@@ -5,6 +5,35 @@ import ical from 'node-ical';
 import ical2json from 'ical2json';
 
 class SSOWebView extends Component {
+    async function parseICal(event) {
+
+        const data = await ical.async.fromURL(event.nativeEvent.data);
+                const iCalJSON = ical2json.convert(data);
+                
+                try {
+                    const response = fetch('https://uqpool.xyz:7777/timetable', {
+                        method: 'POST',
+                        headers: {
+                            accept: 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        body: iCalJSON
+                    });
+
+                    const json = await response.json();
+
+                    if (json.msg =="timetable added") {      
+                        console.log("go back to the home screen");
+                    } else {
+                        console.log("failed to add time table");
+                    }
+
+                } catch (error) {
+                    console.log(error);
+            }
+        
+    }
+    
   render() {
     return 
       <WebView 
@@ -16,33 +45,7 @@ class SSOWebView extends Component {
       }
       
               onMessage={event => {
-              
-              ical.async.fromURL(event.nativeEvent.data, function(err, data) {     
-                      const iCalJSON = ical2json.convert(data);
-                      
-                      try {
-                          const response = fetch('https://uqpool.xyz:7777/timetable', {
-                              method: 'POST',
-                              headers: {
-                                  accept: 'application/json',
-                                  'Content-Type': 'application/json'
-                              },
-                              body: iCalJSON
-                          });
- 
-                          const json = response.json();
-
-                          if (json.msg =="timetable added") {      
-                              console.log("go back to the home screen");
-                          } else {
-                              console.log("failed to add time table");
-                              // Switch to the initial state of the app
-                          }
-
-
-                      } catch (error) {
-                          console.log(error);
-                  }
+                  parseICal(event);
               });
               }}
       />;
@@ -54,6 +57,15 @@ class SSOWebView extends Component {
     const { url } = newNavState;
     if (!url) return;
 
+// handle sid+password uq authenticate page
+    if (url.includes('timetable.my.uq.edu.au/odd/') && !url.includes('student?ss=')) {
+        }    
+        
+        
+        // Handle information sharing confirmation page
+        if (url.includes('timetable.my.uq.edu.au/odd/') && !url.includes('student?ss=')) {
+            }    
+        
     // Handle time table student module selection
     if (url.includes('timetable.my.uq.edu.au/odd/') && !url.includes('student?ss=')) {
         const goToLink = "document.querySelectorAll(\'a[href*=\"student\"]\')";
